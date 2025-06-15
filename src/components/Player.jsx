a42bd4788251e9b745ff61d0aa0beb1eec707897
Toni Mertanen <t.mertanen@hotmail.com>
2025-06-16 01:17:44 +0300

    Refactor Player component to use initialName prop for player name

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,21 +1,28 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick() {
     setIsEditing((editing) => !editing);
   }
 
-  let playerrName = <span className="player-name">{name}</span>;
+  function nameChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
   if (isEditing) {
-    playerrName = <input type="text" value={name} required />;
+    editablePlayerName = (
+      <input type="text" value={playerName} required onChange={nameChange} />
+    );
   }
 
   return (
     <li>
       <span className="player">
-        {playerrName}
+        {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
       </span>
       <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
